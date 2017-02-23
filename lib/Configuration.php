c6e776e53d5c6cb88c7e865bb7b27318ea71833b
Jeanine Jue <jswattonjue@gmail.com>
2017-02-22 17:36:28 -0800

    Updated to 2.0.8

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -523,7 +523,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . phpversion() . PHP_EOL;
         $report .= '    OpenAPI Spec Version: 2.0.0' . PHP_EOL;
-        $report .= '    SDK Package Version: 2.0.6-SNAPSHOT' . PHP_EOL;
+        $report .= '    SDK Package Version: 2.0.8' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;
